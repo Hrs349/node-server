525c9ed2babdb8b055a0e4307682f581a1f6a03c
Hrs349 <140671815+Hrs349@users.noreply.github.com>
2023-10-03 15:30:19 -0400

    review-1

--- listasDeTareas.js
+++ listasDeTareas.js
@@ -1,48 +1,82 @@
 const readlineSync = require("readline-sync");
 
 let listaDeTareas = [];
-
+function crearTarea(){
+  if(listaDeTareas == 0){
+    agregarTarea()
+  }else{
+    console.table(listaDeTareas)
+    agregarTarea()
+  }
+}
 function agregarTarea() {
- console.table(listaDeTareas);
  const Tareas = readlineSync.question(
   "Escriba el numero de numero de tareas, si esta iniciando la lista coloque el numero (1): "
  );
-
  const Descripcion = readlineSync.question("Describa su tarea: ");
-
- listaDeTareas.push({
-  Tareas: parseInt(Tareas),
-  Descripcion,
-  Estado: "Incompleta",
- });
-
- console.log("Nueva tarea agregada con exito ☺");
- console.table(listaDeTareas);
+ if (Tareas === "" || Descripcion === "") {
+  return console.log(" Debe ingresar un numero de tarea y una descripcion");
+ } else {
+  listaDeTareas.push({
+   Tareas: parseInt(Tareas),
+   Descripcion,
+   Estado: "Incompleta",
+  });
+  console.log("Nueva tarea agregada con exito ☺");
+  verlista();
+ }
 }
 
 function eliminartarea(nTarea) {
- console.table(listaDeTareas);
- const Tareasb = readlineSync.question("indique Numero de tarea a eliminar ");
- if (Tareasb - 1 >= 0 && Tareasb - 1 < listaDeTareas.length) {
-  let numtarea = listaDeTareas.filter(
-   (numero) => numero.Tareas !== (nTarea = parseInt(Tareasb))
-  );
-
-  listaDeTareas = numtarea;
-  console.table(listaDeTareas);
-  console.log(`tarea numero ${Tareasb} eliminada`);
+ if (listaDeTareas == 0) {
+  console.log("No se han agregado tareas para poder eliminar");
+  barramenu();
  } else {
-  console.error(`El numero de tarea ${Tareasb} no esta en la lista`);
+  const Tareasb = readlineSync.question("indique Numero de tarea a eliminar ");
+  if (Tareasb - 1 >= 0 && Tareasb - 1 < listaDeTareas.length) {
+   let numtarea = listaDeTareas.filter(
+    (numero) => numero.Tareas !== (nTarea = parseInt(Tareasb))
+   );
+   listaDeTareas = numtarea;
+   verlista();
+   console.log(`tarea numero ${Tareasb} eliminada`);
+  } else {
+   console.error(`El numero de tarea ${Tareasb} no esta en la lista`);
+  }
  }
 }
 function completarTarea() {
- console.table(listaDeTareas);
-
- const TareasC = readlineSync.question("indique Numero de tarea a completar ");
-
- listaDeTareas[TareasC - 1].Estado = "completada";
- console.table(listaDeTareas);
- console.log(`tarea numero ${TareasC} completada`);
+ if (listaDeTareas == 0) {
+  console.log("Debe agregar una tarea para poder completarla");
+  return barramenu();
+ } else {
+  const TareasC = readlineSync.question("indique Numero de tarea a completar ");
+  listaDeTareas[TareasC - 1].Estado = "completada";
+  console.table(listaDeTareas);
+  console.log(`tarea numero ${TareasC} completada`);
+ }
+}
+function verlista() {
+ if (listaDeTareas == 0) {
+  console.log("no se han agregador tareas");
+ } else console.table(listaDeTareas);
+ barramenu();
+}
+function salirApp() {
+  return new Promise((resolve, reject) => {
+   console.log("Saliendo de la aplicacion...");
+   setTimeout(() => {
+    resolve(console.log("Se ha cerrado la aplicacion exitosamente"));
+   }, 3000);
+  });
+ }
+function abrir() {
+ return new Promise((resolve, reject) => {
+  console.log("Cargando aplicacion...");
+  setTimeout(() => {
+   resolve(barramenu());
+  }, 4000);
+ });
 }
 
 function barramenu() {
@@ -56,11 +90,11 @@ function barramenu() {
  const opciones = readlineSync.question(`elija una opcion del menu:`);
  switch (opciones) {
   case "1":
-   console.table(listaDeTareas);
+   verlista();
    barramenu();
    break;
   case "2":
-   agregarTarea();
+   crearTarea();
    barramenu();
    break;
   case "3":
@@ -72,6 +106,7 @@ function barramenu() {
    barramenu();
    break;
   case "5":
+   salirApp();
    break;
   default:
    console.error("esa opcion no es valida");
@@ -80,4 +115,4 @@ function barramenu() {
  }
 }
 
-barramenu();
+abrir();
---
+++ review-1.md
@@ -0,0 +1,9 @@
+#¿Qué es mi producto y para que sirve?
+
+Mi producto es una pequeña app que puede ayudar a gestionar el control de actividades
+o metas por medio de tareas, en el cual las personas puede describir o anotar las tareas necesarias para un fin o meta
+
+#¿Cuáles son las funcionalidades más importantes y porque los usuarios las usarían?
+
+Esta applicacion esta diseñada para la creacion de nua lista de tareas, la cual podras crear las tareas y manejar su funcionalidad, en la cual podras editarlas por cualquier error que se pueda cometer al momento de escribir, ver y actualizar su estatus si esta completa o incompleta, 
+tambien  si lo requiere podra eliminar las tareas que sean cargadas por error o las que ya no se requieran visualizar. 
